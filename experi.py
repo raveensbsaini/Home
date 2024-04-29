bd920adda309047da429aeb6b1204f46f0ca562e
ravindra kumar saini <raveensbsaini@gmail.com>
2024-04-29 09:39:16 +0530

    update tee.py

--- experi.py
+++
@@ -1,3 +0,0 @@
-import sys
-print("ankit",file=sys.stdout)
-print("ravi",file=sys.stderr)
--- tee.py
+++ tee.py
@@ -1,18 +1,30 @@
 import os,sys
-filefolder = os.getcwd()
+from pathlib import Path
+filepath = os.getcwd()
+if len(sys.argv) < 2:
+    sys.exit("tee.py command except 2 or more than two commands.")
+filepath += "/{}".format(sys.argv[1])
 if len(sys.argv) == 2:
-    if sys.argv[1] == "--help":
-        print("commands syntex :","tee {write here} {path to file} :",end="" )
-        print("open {filename} append file and print file to terminal")
-if len(sys.argv) != 3:
-    print("tee commands except 3 agrument but {} is given".format(len(sys.argv)))
-else:
-    message = sys.argv[1]
-    filelocation = filefolder + f"/{sys.argv[2]}" 
-    file = open(filelocation,"a")
-    file.write(sys.argv[1])
+    if sys.argv[0] != "tee.py":
+        sys.exit("1st command argument must be tee.py but {} is given.".format(sys.argv[0]))
+    file = open(filepath,"a")
+    for line in sys.stdin:
+        file.write(line)
     file.close()
-    file = open(filelocation,'r')
+    sys.exit("success")
+if len(sys.argv) == 3:
+    if sys.argv[0] != "tee.py":
+        sys.exit("1st comman argument must be tee.py but {} is given ".format(sys.argv[0]))
+    try:
+        file = open(filepath,"a")
+    except:
+        sys.exit("cannot find file {}".format(sys.argv[1]))
+    pattern = sys.argv[2]
+    # file.write("\n")
+    file.write(pattern)
+    file.write("\n")
+    file.close
+    file = open(filepath,"r")
     for i in file:
-        print(i)
+        print(i,end="")
     file.close()
