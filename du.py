27d7dca9fcdf976228a7797e2ec0e14e4e667a48
ravindra kumar saini <raveensbsaini@gmail.com>
2024-05-01 18:45:46 +0530

    make changes in tee.py file

--- du.py
+++ du.py
@@ -1,10 +1,14 @@
 import sys,os
 filepath = os.getcwd()
+# print(filepath)
 if len(sys.argv) != 2:
     sys.exit("du.py commands except 2 argument but {} is Given.".format(len(sys.argv)))
 if sys.argv[0] != "du.py":
     sys.exit("first commands argument must be du.py but {} is given.".format(sys.argv[0]))
 filepath += "/{}".format(sys.argv[1])
+if sys.argv[1][0] == "/":
+    filepath = sys.argv[1]
+# print(filepath)
 file = open(filepath,"rb")
 count = 0
 while file.read(1):
--- tee.py
+++ tee.py
@@ -4,19 +4,22 @@ filepath = os.getcwd()
 if len(sys.argv) < 2:
     sys.exit("tee.py command except 2 or more than two commands.")
 filepath += "/{}".format(sys.argv[1])
+if sys.argv[1][0] == "/":
+    filepath = sys.argv[1]
 if len(sys.argv) == 2:
     if sys.argv[0] != "tee.py":
         sys.exit("1st command argument must be tee.py but {} is given.".format(sys.argv[0]))
-    file = open(filepath,"a")
+    file = open(filepath,"w")
     for line in sys.stdin:
         file.write(line)
+        print(line,end="")
     file.close()
-    sys.exit("success")
+    sys.exit(0)
 if len(sys.argv) == 3:
     if sys.argv[0] != "tee.py":
         sys.exit("1st comman argument must be tee.py but {} is given ".format(sys.argv[0]))
     try:
-        file = open(filepath,"a")
+        file = open(filepath,"w")
     except:
         sys.exit("cannot find file {}".format(sys.argv[1]))
     pattern = sys.argv[2]
